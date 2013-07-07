ce9de5173d1c133c5343f54f4aa4ce2f4b7e12d4
Yunohost <admin@yunohost.org>
2013-07-07 15:41:35 +0200

    Test build: tahoe conf bugfix

--- yunohost-config-tahoe/config/backup/configure_tahoe.sh
+++ yunohost-config-tahoe/config/backup/configure_tahoe.sh
@@ -4,7 +4,11 @@ CONF="/usr/share/yunohost/yunohost-config/backup/tahoe.cfg"
 HOME_SIZE=$(df /home | sed -n 2p | awk '{print $2}')
 HOME_SIZE_LEFT=$(df /home | sed -n 2p | awk '{print $4}')
 BACKUP_SIZE=$(($HOME_SIZE/10))
-DOMAIN=$(cat /etc/yunohost/current_host)
+DOMAIN='yunohost.org'
+if [ -f /etc/yunohost/current_host ];
+then
+    let DOMAIN=$(cat /etc/yunohost/current_host)
+fi
 sed -i "s/nameToChange/$DOMAIN/g" "$CONF"
 if [ $HOME_SIZE -gt 10000000 ] && [ $HOME_SIZE_LEFT -gt $BACKUP_SIZE ];
 then
