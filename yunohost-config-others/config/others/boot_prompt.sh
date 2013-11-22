430ea8dc0703781ccfc72c55edc20918c193204c
Yunohost <admin@yunohost.org>
2013-11-22 15:48:39 +0100

    Test build: update boot_prompt

--- yunohost-config-others/config/others/boot_prompt.sh
+++ yunohost-config-others/config/others/boot_prompt.sh
@@ -111,27 +111,27 @@ if [[ ! -f /etc/yunohost/installed ]]
 then
         if [[ ! -f /etc/yunohost/from_script ]]
         then
-		echo -e "\n mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Yunohost v2" > /etc/issue
-		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " >> /etc/issue
-		echo -E " mmmQ                       Ymmmm " >> /etc/issue
-		echo -E " mmm#   .2A929     .12iQ7   :mmmm " >> /etc/issue
-		echo -E " mmmp    ;mmmm#   :mmmmp.   ,mmmm " >> /etc/issue
-		echo -E " mmm#     ,mmmQ5 .Ymmmp     :mmmm " >> /etc/issue
-		echo -E " mmmp      ,mmmp ,mmmp      ,mmmm " >> /etc/issue
-		echo -E " mmm#       ;mmmmNmmp       :mmmm " >> /etc/issue
-		echo -E " mmmp       .YmmmmmA;       ,mmmm " >> /etc/issue
-		echo -E " mmm#        .KmmmQY        :mmmm " >> /etc/issue
-		echo -E " mmmp         :mmm#         ,mmmm " >> /etc/issue
-		echo -E " mmm#        .7mmmp,        :mmmm " >> /etc/issue
-		echo -E " mmmp         7mmm#,        ,mmmm " >> /etc/issue
-		echo -E " mmm#                       :mmmm " >> /etc/issue
-		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " >> /etc/issue
-		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " >> /etc/issue
+		echo -e "\n mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm Yunohost v2"
+		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " 
+		echo -E " mmmQ                       Ymmmm " 
+		echo -E " mmm#   .2A929     .12iQ7   :mmmm " 
+		echo -E " mmmp    ;mmmm#   :mmmmp.   ,mmmm " 
+		echo -E " mmm#     ,mmmQ5 .Ymmmp     :mmmm " 
+		echo -E " mmmp      ,mmmp ,mmmp      ,mmmm " 
+		echo -E " mmm#       ;mmmmNmmp       :mmmm " 
+		echo -E " mmmp       .YmmmmmA;       ,mmmm " 
+		echo -E " mmm#        .KmmmQY        :mmmm " 
+		echo -E " mmmp         :mmm#         ,mmmm " 
+		echo -E " mmm#        .7mmmp,        :mmmm " 
+		echo -E " mmmp         7mmm#,        ,mmmm " 
+		echo -E " mmm#                       :mmmm " 
+		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " 
+		echo -E " mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm " 
 		echo -e "\n          \e[0;30;47m Post-installation \e[0m\n"
                 read -p "Proceed to post-installation? (y/n) " -n 1
 		RESULT=1
 		while [ $RESULT -gt 0 ]; do
-			if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+			if [[ $REPLY =~ ^[Nn]$ ]]; then
 				exit 0
 			fi
                         echo -e "\n"
