acb7d34253f16a8c0de7cc4352f340b3055d1c08
Yunohost <admin@yunohost.org>
2013-12-01 22:23:59 +0100

    Production build: Update default folder

---
+++ yunohost-config-dovecot/config/dovecot/sa-learn-pipe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo /usr/bin/sa-learn $* /tmp/sendmail-msg-$$.txt
+echo "$$-start ($*)" >> /tmp/sa-learn-pipe.log
+
+#echo $* > /tmp/sendmail-parms.txt
+cat<&0 >> /tmp/sendmail-msg-$$.txt
+
+/usr/bin/sa-learn $* /tmp/sendmail-msg-$$.txt
+
+rm -f /tmp/sendmail-msg-$$.txt
+
+echo "$$-end" >> /tmp/sa-learn-pipe.log
+
+exit 0
