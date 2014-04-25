f95e92582693aa65953f231344910c79efc7d51e
Yunohost <admin@yunohost.org>
2014-04-25 19:39:39 +0200

    Test build: [fix] Check directory existance instead of file

--- udiskie/setup.py
+++ udiskie/setup.py
@@ -1,29 +1,11 @@
 # encoding: utf-8
-from setuptools import setup
-from setuptools.command.install import install
+#!/usr/bin/env python
+
+from distutils.core import setup
 from subprocess import call
 import sys
 import logging
 from os import path
-
-# check availability of runtime dependencies
-def check_any(*packages):
-    """Issue a warning if none of the packages is available."""
-    errors = []
-    for package in packages:
-        try:
-            __import__(package)
-            return True
-        except ImportError:
-            errors.append(sys.exc_info()[1])
-    logging.warn("\n\t".join(["Missing runtime dependencies:"]
-                             + [str(e) for e in errors]))
-    return False
-
-check_any('dbus')
-check_any('gobject')
-check_any('pynotify', 'notify2')
-check_any('gtk')
 
 # read long_description from README.rst
 long_description = None
@@ -41,15 +23,6 @@ icon_classes = {'actions': ('mount', 'unmount',
                             'lock', 'unlock',
                             'eject', 'detach')}
 
-class custom_install(install):
-    def run(self):
-        install.run(self)
-        try:
-            # ignore failures since the tray icon is an optional component:
-            call(['gtk-update-icon-cache', theme_base])
-        except OSError:
-            logging.warn(sys.exc_info()[1])
-
 setup(
     name='udiskie',
     version='0.6.4',
@@ -61,7 +34,6 @@ setup(
     maintainer_email='t_glaessle@gmx.de',
     url='https://github.com/coldfix/udiskie',
     license='MIT',
-    cmdclass={'install': custom_install},
     packages=[
         'udiskie',
     ],
@@ -79,9 +51,6 @@ setup(
             'udiskie-mount = udiskie.cli:Mount.main',
             'udiskie-umount = udiskie.cli:Umount.main',
         ],
-    },
-    extras_require={
-        'notifications': ['notify2']
     },
     install_requires=[
         # Currently not building out of the box:
